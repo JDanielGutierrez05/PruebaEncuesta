97f05c30837bab7b33fcebdc25ec9b2ed4c8a0f9
Juan Daniel Gutierrez <jgutierrez@azimutenergia.co>
2021-12-27 18:43:54 -0500

    wip: add response from service file

--- src/main/java/com/encuestas/controller/Polls.java
+++ src/main/java/com/encuestas/controller/Polls.java
@@ -1,6 +1,8 @@
 package com.encuestas.controller;
 
+import com.encuestas.services.PollServices;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("polls")
 public class Polls {
+    private final PollServices pollServices;
 
-    @GetMapping(produces = "application/json")
-    public @ResponseBody String getBook() {
-        log.info("Hi Log");
-        return "Hi";
+    @Autowired
+    public Polls (PollServices pollServices) {
+        this.pollServices = pollServices;
     }
 
+    @GetMapping(produces = "application/json")
+    public @ResponseBody String getPolls() {
+        return pollServices.poll();
+    }
 }
---
+++ src/main/java/com/encuestas/services/PollServices.java
@@ -0,0 +1,11 @@
+package com.encuestas.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PollServices {
+
+    public String poll(){
+        return "Hi in services";
+    }
+}
